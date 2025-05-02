39ec25bee2976e3c4db12414e8da34082741692e
iara-szw <iszwarstein@gmail.com>
2025-05-02 20:44:53 -0300

    Carga 5 albumes

--- Models/Catalogo.cs
+++ Models/Catalogo.cs
@@ -29,30 +29,32 @@ class Catalogo
         foto ="footloose.jpg";
         crearAlbum(nombre, artista, productor, genero, temas, foto, id);
 
-        nombre = "Album3";
-        artista = new Artista("nombreArtista3", "descripcion3");
-        productor = "Productor3";
-        genero = "Genero3";
-        temas = new List<Cancion> { new Cancion("nombre", 3.15), new Cancion("nombre2", 3.15), new Cancion("nombre3", 3.15) };
-        id = 125;
-        foto = "AlbumPortada2.png";
+              nombre = "Infinitely Ordinary";
+        artista = new Artista("The Wrecks", "Álbum debut de la banda estadounidense The Wrecks, lanzado en 2020. Combina elementos del indie rock y pop punk con letras sobre juventud y ansiedad moderna.");
+        productor = "The Wrecks";
+        genero = "Indie Rock / Pop Punk";
+        temas = new List<Cancion> {new Cancion("Freaking Out", 3.10),new Cancion("Feels So Nice", 3.28),new Cancion("Out of Style", 2.56),new Cancion("Fvck Somebody", 3.03),new Cancion("Four", 3.26), new Cancion("We All Get Lonely", 3.17),new Cancion("This Life I Have", 2.52), new Cancion("Infinitely Ordinary", 2.46)};
+        id = 003;
+        foto = "infinitelyOrdinary.jpg";
         crearAlbum(nombre, artista, productor, genero, temas, foto, id);
 
-        nombre = "Album4";
-        artista = new Artista("nombreArtista3", "descripcion3");
-        productor = "Productor3";
-        genero = "Genero3";
-        temas = new List<Cancion> { new Cancion("nombre", 3.15), new Cancion("nombre2", 3.15), new Cancion("nombre3", 3.15) };
-        id = 126;
-        foto = "AlbumPortada2.png";
+        nombre = "The Chaos Chapter: FREEZE";
+        artista = new Artista("Tomorrow X Together", "También conocido como TXT, este grupo de K-pop presenta en su segundo álbum de estudio una mezcla de géneros con letras que exploran el amor, la desesperación y la juventud en tiempos modernos.");
+        productor = "Slow Rabbit, Aaron Hibell, El Capitxn, entre otros";
+        genero = "K-pop / Pop Rock / Synth Pop";
+        temas = new List<Cancion> {new Cancion("Anti-Romantic", 3.37), new Cancion("0X1=LOVESONG (I Know I Love You) feat. Seori", 3.22), new Cancion("Magic", 2.59), new Cancion("Ice Cream", 3.33), new Cancion("What If I Had Been That PUMA", 3.30), new Cancion("No Rules", 3.09), new Cancion("Dear Sputnik", 3.16), new Cancion("Frost", 2.49)};
+        id = 004;
+        foto = "Freeze.jpeg";
         crearAlbum(nombre, artista, productor, genero, temas, foto, id);
-                nombre = "Album5";
-        artista = new Artista("nombreArtista3", "descripcion3");
-        productor = "Productor3";
-        genero = "Genero3";
-        temas = new List<Cancion> { new Cancion("nombre", 3.15), new Cancion("nombre2", 3.15), new Cancion("nombre3", 3.15) };
-        id = 127;
-        foto = "AlbumPortada2.png";
+
+
+        nombre = "Speak Now (Taylor’s Version)";
+        artista = new Artista("Taylor Swift", "Regrabación del álbum original de 2010. En esta versión de 2023, Taylor recupera el control de sus masters e incluye temas 'From The Vault' con nuevos colaboradores.");
+        productor = "Taylor Swift, Jack Antonoff";
+        genero = "Country Pop / Pop Rock";
+        temas = new List<Cancion> {new Cancion("Mine", 3.50), new Cancion("Sparks Fly", 4.21), new Cancion("Back to December", 4.55), new Cancion("Speak Now", 4.01), new Cancion("Dear John", 6.44), new Cancion("Mean", 3.57), new Cancion("The Story of Us", 4.25), new Cancion("Never Grow Up", 4.50), new Cancion("Enchanted", 5.52), new Cancion("Better Than Revenge", 3.37), new Cancion("Innocent", 5.01), new Cancion("Haunted", 4.02), new Cancion("Last Kiss", 6.07), new Cancion("Long Live", 4.19), new Cancion("Ours", 3.58), new Cancion("Superman", 4.42), new Cancion("Electric Touch (feat. Fall Out Boy)", 3.53), new Cancion("When Emma Falls in Love", 4.14), new Cancion("I Can See You", 4.05), new Cancion("Castles Crumbling (feat. Hayley Williams)", 4.56), new Cancion("Foolish One", 3.44), new Cancion("Timeless", 4.30)};
+        id = 005;
+        foto = "speakNow.jpg";
         crearAlbum(nombre, artista, productor, genero, temas, foto, id);
     }
 
